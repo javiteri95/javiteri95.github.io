fa27d91de9d63318090dc751ad55061f935b07aa
Jose Viteri <josevns95@gmail.com>
2026-05-05 23:39:03 +0200

    solve deployment.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         secondary: '#2FA084',
         highlight: '#6FCF97',
       },
+    // colors: {
+    //     background: '#FFF2E0',
+    //     primary: '#898AC4',
+    //     secondary: '#A2AADB',
+    //     highlight: '#C0C9EE',
+    // },
+    // colors: {
+    //     background: '#BDE8F5',
+    //     primary: '#0F2854',
+    //     secondary: '#1C4D8D',
+    //     highlight: '#4988C4'
+    // },
       fontFamily: {
         sans: ['Inter', 'system-ui', 'sans-serif'],
         mono: ['JetBrains Mono', 'monospace'],
